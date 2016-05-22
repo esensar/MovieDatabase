e4f3fb916a0e4115de0772279f22541f601f9c17
esensar <esarajcic1@etf.unsa.ba>
2016-05-22 16:58:34 +0200

    Phping

---
+++ add.php
@@ -0,0 +1,19 @@
+<?php include 'gori.php';?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML>
+    <?php include 'header.php'; ?>
+    <div class="content">
+      <form class="contactForm" method="post">
+        <label >
+          Slika link:
+        </label><br />
+        <input type="text" id="emailField" name="slika"/> <br />
+        <label >
+          Tekst:
+        </label><br />
+        <textarea type="text" rows="30" cols="70" id="passwordField" name="vijest" ></textarea><br />
+        <input type="submit" name="submit" value="Submit" />
+      </form>
+    </div>
+  </BODY>
+</HTML>
