846b120b3434c7bc5c23b78f9c6ede34cfa6f603
esensar <esarajcic1@etf.unsa.ba>
2016-06-01 00:01:09 +0200

    Last newline test

--- gori.php
+++ gori.php
@@ -15,8 +15,7 @@
   if(isset($_POST['vijest'])) {
     $vijestiFile = file_get_contents($openShiftFolder."vijesti.csv");
     $vijest = $_POST['vijest'];
-    $vijest = str_replace("\n", "<br>", $vijest);
-    if(strpos($vijest, ',') !== FALSE) {
+    if(strpos($vijest, ',') !== FALSE || strpos($vijest, '\n') !== FALSE) {
       $vijest = str_replace('"', '""', $vijest);
       $vijest = '"'.$vijest.'"';
     }
